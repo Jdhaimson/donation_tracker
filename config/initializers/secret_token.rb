7306f11e2437e3c47d0c640610eef409ba32fd9d
Josh Haimson <jdhaimson@gmail.com>
2013-02-23 16:12:29 -0500

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DonationTracker::Application.config.secret_token = '7574e42693fa66af95ceb0aaf33485477f698d0365e0b95a4e7d77babafe7257d5bddf717fc980d0969f91979e6665234f54afe8657fdf1d58d837ecf1042184'
